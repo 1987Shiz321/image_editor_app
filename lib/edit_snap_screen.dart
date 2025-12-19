d95ff5f00ed1fcc3a27c6a9476af0d103fddde76
TouchKot723 <192498178+1987Shiz321@users.noreply.github.com>
2025-12-19 13:46:20 +0900

    6.7 画像を編集する処理の実装(回転・左右反転)

--- lib/edit_snap_screen.dart
+++ lib/edit_snap_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:image_editor_app/l10n/app_localizations.dart';
 import 'package:image_editor_app/gen/assets.gen.dart';
+import 'package:image/image.dart' as image_lib;
 
 class ImageEditScreen extends StatefulWidget {
   const ImageEditScreen({super.key, required this.imageBitmap});
@@ -13,6 +14,41 @@ class ImageEditScreen extends StatefulWidget {
 }
 
 class _ImageEditScreenSatte extends State<ImageEditScreen> {
+  late Uint8List _imageBitmap;
+
+  @override
+  void initState() {
+    super.initState();
+    _imageBitmap = widget.imageBitmap;
+  }
+
+  void _rotateImage() {
+    //画像のデコード
+    final image = image_lib.decodeImage(_imageBitmap);
+    if (image == null) return;
+    //画像を時計回りに90度回転
+    final rotateImage = image_lib.copyRotate(image, angle: 90);
+    //画像をエンコードして状態更新
+    setState(() {
+      _imageBitmap = image_lib.encodeBmp(rotateImage);
+    });
+  }
+
+  void _flipImage() {
+    //画像のデコード
+    final image = image_lib.decodeImage(_imageBitmap);
+    if (image == null) return;
+    // 画像を水平方向に反転する
+    final flipImage = image_lib.copyFlip(
+      image,
+      direction: image_lib.FlipDirection.horizontal,
+    );
+    //画像をエンコードして状態更新
+    setState(() {
+      _imageBitmap = image_lib.encodeBmp(flipImage);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final l10n = L10n.of(context);
@@ -25,13 +61,13 @@ class _ImageEditScreenSatte extends State<ImageEditScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.memory(widget.imageBitmap),
+            Image.memory(_imageBitmap),
             IconButton(
-              onPressed: () {},
+              onPressed: () => _rotateImage(),
               icon: Assets.rotateIcon.svg(width: 24, height: 24),
             ),
             IconButton(
-              onPressed: () {},
+              onPressed: () => _flipImage(),
               icon: Assets.flipIcon.svg(width: 24, height: 24),
             ),
           ],
